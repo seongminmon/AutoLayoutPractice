a60ec8704b91e13593989f8d444d832dff333757
seongmin kim <k2417000@naver.com>
2024-05-20 19:32:43 +0900

    배달의 민족 화면 구성

--- AutoLayoutPractice/BeMinViewController.swift
+++ AutoLayoutPractice/BeMinViewController.swift
@@ -8,8 +8,14 @@
 import UIKit
 
 class BeMinViewController: UIViewController {
-
+    
+    @IBOutlet var backgroundView: UIView!
+    @IBOutlet var textFieldView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        backgroundView.layer.cornerRadius = 20
+        textFieldView.layer.cornerRadius = 10
     }
 }
